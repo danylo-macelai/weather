8050d7acb53c7b11f9f362137924e72ad42e2eeb
danylo-macelai <danylomacelai@gmail.com>
2021-03-06 23:23:34 -0300

    Added application layout

---
+++ src/weather/Weather.svelte
@@ -0,0 +1,50 @@
+<script>
+</script>
+
+<main class="home__content">
+	<aside>//Side Bar</aside>
+	<article>
+		// Content Heading
+		<section>// Content Area</section>
+	</article>
+</main>
+
+<style>
+	main {
+		display: flex;
+		text-align: center;
+		justify-content: center;
+		max-inline-size: 1440px;
+		margin: auto;
+		line-height: 19px;
+		height: 100vh;
+	}
+
+	aside {
+		width: 459px;
+		float: left;
+		background-color: #1e213a;
+	}
+
+	article {
+		width: 981px;
+		float: left;
+		background-color: #100e1d;
+	}
+
+	@media screen and (max-width: 1024px) {
+		.home__content {
+			flex-direction: column;
+		}
+
+		aside {
+			height: 810px;
+			width: 100%;
+		}
+
+		article {
+			height: 100%;
+			width: 100%;
+		}
+	}
+</style>
---
+++ src/weather/index.js
@@ -0,0 +1,5 @@
+import Weather from "./Weather.svelte";
+
+export {
+    Weather,
+};
